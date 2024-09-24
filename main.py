6ba7b3637b77b3b43d13886a0b03b89210af96a3
Elkin-Ferney-Rodriguez <elkinferney@misena.edu.co>
2024-09-24 15:59:49 +0000

    Tablas de multiplicar

--- main.py
+++ main.py
@@ -1,50 +1,25 @@
-# Formulas Python
-def operacion(opcion, a, b):
-    match opcion:
-        case "1":
-            return a + b
-        case "2":
-            return a - b
-        case "3":
-            return a * b
-        case "4":
-            if b != 0:
-                return a / b
-            else:
-                return "Dividir por cero es indefinido."
-        case _:
-            return "Escoja una de las opciones."
+def tabla_multiplicar(numero):
+    numero_mostrar = int(numero) if numero.is_integer() else numero
+    print(f"\nTabla de multiplicar del {numero_mostrar}:")
+    for i in range(1, 11):
+        resultado = numero * i
+
+        if resultado.is_integer():
+            resultado = int(resultado)  
+        print(f"{numero_mostrar} x {i} = {resultado}")
 
 while True:
-    print("Bienvenido")
-    print("Escoja la operación que necesita:")
-    print("1. Suma")
-    print("2. Resta")
-    print("3. Multiplicación")
-    print("4. División")
-    print("5. Salir")
+    print("Tablas de multiplicar")
+    entrada = input("Ingrese un número para ver su tabla de multiplicar: ").strip()
+    entrada = entrada.replace(',', '.')  
+    try:
+        numero = float(entrada)  
+    except ValueError:
+        print("Entrada no válida. Por favor, ingrese un número.")
+        continue
+    tabla_multiplicar(numero)
 
-    opcion = input("Digite su opción: ")
-
-    if opcion == "5":
+    continuar = input("¿Desea ver otra tabla? (si/no): ").strip().lower()
+    if continuar != "si":
         print("Gracias por utilizar el programa. ¡Hasta luego!")
         break
-
-    if opcion in ["1", "2", "3", "4"]:
-        a = float(input("Digite el primer número: "))
-        b = float(input("Digite el segundo número: "))
-
-        resultado = operacion(opcion, a, b)
-        
-        if isinstance(resultado, float) and resultado.is_integer():
-            resultado = int(resultado)  
-
-        print(f"El resultado de : {resultado}")
-        
-        # Preguntar si desea realizar otra operación
-        continuar = input("¿Desea realizar otra operación? (si/no): ").strip().lower()
-        if continuar != "si":
-            print("Gracias por utilizar el programa. ¡Hasta luego!")
-            break  # Salir del bucle principal
-    else:
-        print("Escoja una de las opciones válidas.")
